77bbddfed2cd524c428cb2adf8cc35cba8dcef53
David Leonard <david.leonard@flexential.com>
2020-05-03 15:34:16 -0500

    initial provider setup.

---
+++ input.tf
@@ -0,0 +1,19 @@
+variable "vcd_user" {
+  type = string
+}
+
+variable "vcd_pass" {
+  type = string
+}
+
+variable "vcd_org" {
+  type = string
+}
+
+variable "vcd_vdc" {
+  type = string
+}
+
+variable "vcd_url" {
+  type = string
+}
---
+++ vcd-connection.tf
@@ -0,0 +1,9 @@
+provider "vcd" {
+  user                 = var.vcd_user
+  password             = var.vcd_pass
+  org                  = var.vcd_org
+  vdc                  = var.vcd_vdc
+  url                  = var.vcd_url
+  max_retry_timeout    = 90
+  allow_unverified_ssl = false
+}
